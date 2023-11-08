7075f125593a6543643234b0cddf9f2a9bb01511
AntonyWangai <136501053+AntonyWangai@users.noreply.github.com>
2023-11-08 16:55:51 +0300

    Update TriangleArea.java

--- src/math/TriangleArea.java
+++ src/math/TriangleArea.java
@@ -8,13 +8,10 @@ public class TriangleArea {
 		
 		System.out.print("Enter the height:");
 		double num2=in.nextInt();
-		
-<<<<<<< HEAD
-		double area= 0*5*(num1*num2);
-=======
+
 		double area= 0.5*(num1*num2);
->>>>>>> df68b7b2324184c0d1db218c47f2a3ec566df537
-		System.out.println("The area is"+area);
+
+		System.out.println("The area is: "+area);
 		
 		
 	}
