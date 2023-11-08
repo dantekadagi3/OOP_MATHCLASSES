c4c9e89f2183b17486bde9c3e61b80ac2c85efa1
kyalo-45 <117834094+kyalo-45@users.noreply.github.com>
2023-11-08 09:44:08 +0300

    Update TriangleArea.java

--- src/math/TriangleArea.java
+++ src/math/TriangleArea.java
@@ -1,10 +1,24 @@
 package math;
 
+import java.util.Scanner;
+
 public class TriangleArea {
-	    public double calculateArea(double base, double height) {
-	        return 0.5 * base * height;
-	    }
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the base of the triangle: ");
+        double base = scanner.nextDouble();
 
+        System.out.print("Enter the height of the triangle: ");
+        double height = scanner.nextDouble();
 
+        double area = calculateArea(base, height);
+        System.out.println("Triangle Area: " + area);
+
+        scanner.close();
+    }
+
+    public static double calculateArea(double base, double height) {
+        return 0.5 * base * height;
+    }
+}
