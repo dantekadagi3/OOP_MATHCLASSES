43584cd31eaf2d935e4b90e9c0ce155967ceecb5
kyalo-45 <117834094+kyalo-45@users.noreply.github.com>
2023-11-08 07:31:57 +0300

    Update MainClass.java

--- src/math/MainClass.java
+++ src/math/MainClass.java
@@ -6,6 +6,12 @@ public class MainClass {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Members");
 
+		double base = 5.0;
+    		double height = 4.0;
+
+	        double area = TriangleArea.calculateArea(base, height);
+	        System.out.println("Triangle Area: " + area);
+
 	}
 
 }
